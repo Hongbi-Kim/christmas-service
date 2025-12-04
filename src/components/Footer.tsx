d556f7d510c861f878e82b9fd45f17f8b93c1015
Hongbi-Kim <hongbi1620@gmail.com>
2025-12-04 23:35:18 +0900

    change web site

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export default function Footer({ className = '' }: FooterProps) {
 
           {/* 오른쪽: Powered by Wave X */}
           <a
-            href="https://www.instagram.com/dreambus_dev.zip?igsh=MWNvaXFod2lhZzN2aQ%3D%3D&utm_source=qr"
+            href="https://www.wavetoai.com"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-1.5 hover:text-blue-600 transition-colors"
